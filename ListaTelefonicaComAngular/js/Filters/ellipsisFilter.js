7be1e0ee54dd19a17157d89fae2e796ae76be146
mikemajesty <mikemajesty2011@gmail.com>
2016-01-25 21:25:27 -0200

    Aula 09 - Criando Filtros

---
+++ ListaTelefonicaComAngular/js/Filters/ellipsisFilter.js
@@ -0,0 +1,9 @@
+﻿angular.module("listaTelefonica").filter("ellipsis",function () {
+    return function (input, size) {
+        if (input.length <= size) {
+            return input;
+        }
+        var output = input.substring(0, size) + "...";
+        return output;
+    }
+});
---
+++ ListaTelefonicaComAngular/js/Filters/nameFilter.js
@@ -0,0 +1,11 @@
+﻿angular.module("listaTelefonica").filter("name", function () {
+
+    return function (input) {
+        var lista = input.split(" ");
+        var cont = 1;
+        var lisFormatada = lista.map(function (nome) {
+            return nome.charAt(0).toUpperCase() + nome.substring(1);
+        });
+        return lisFormatada.join(" ");
+    }
+});
