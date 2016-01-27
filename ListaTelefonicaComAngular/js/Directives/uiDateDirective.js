0ef54092c00e1222eb76c95394edbc5a248de11b
mikemajesty <mikemajesty2011@gmail.com>
2016-01-26 22:52:14 -0200

    Aula 11 - Criando Diretivas - Parte 2

---
+++ ListaTelefonicaComAngular/js/Directives/uiDateDirective.js
@@ -0,0 +1,26 @@
+﻿angular.module("listaTelefonica").directive("uiDate", function () {
+    return {
+        required: "ngModel",
+        link: function (scope, element, attrs, ctrl) {
+
+            var _formatDate = function (date) {
+                date = date.replace(/[^0-9]+/g, "");
+                if (date.length > 2) {
+                    date = date.substring(0, 2) + "/" + data.substring(2);
+                }
+                if (date.length > 5) {
+                    date = date.substring(0, 5) + "/" + data.substring(5,8);
+                }
+                return date;
+            }
+            console.log(scope);
+            console.log(element);
+            console.log(attrs);
+            element.bind("keyup", function () {
+                ctrl.$setViewValue(_formatDate(ctrl.$viewValue));
+                ctrl.$render();
+            });
+
+        }
+    };
+});
